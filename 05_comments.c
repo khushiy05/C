8633685e335986819e273b026c7b7b5e4802aa9d
khushiy05 <khushiyewale150@gmail.com>
2025-08-27 19:16:43 +0530

    comments

---
+++ 05_comments.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    // this is single line comments for writing any sentence . like for header/heading
+    printf("Hello World");
+    //this is return sentence
+    /*............... ==> this is multi line comments */
+    return 0;
+
+}
