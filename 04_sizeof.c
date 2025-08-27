6735e225e216581bac8b97f2ffba42fe3e413a26
khushiy05 <khushiyewale150@gmail.com>
2025-08-27 19:06:46 +0530

    size of c

---
+++ 04_sizeof.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("Size od int: %zu bytes\n", sizeof(int));
+    printf("Size od char: %zu bytes\n", sizeof(char));
+    printf("Size od float: %zu bytes\n", sizeof(float));
+    return 0;
+    
+}
